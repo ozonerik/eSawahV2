d17c2cbcbc3584deb5679532e4b2e2d58a5fbd15
ozonerik <ozonerik@gmail.com>
2022-04-08 13:24:55 +0700

    change view verify

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,43 +1,35 @@
-@extends('layouts.guest')
+@extends('layouts.verify')
 
 @section('content')
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="/" class="h1"><b>Admin</b>LTE</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">{{ __('Verify Your Email Address') }}</p>
+<!-- Default box -->
+<div class="card">
+  <div class="card-header">
+    <h3 class="card-title">{{ __('Verify Your Email Address') }}</h3>
+  </div>
+  <div class="card-body">
       @if (session('resent'))
         <div class="alert alert-success" role="alert">
             {{ __('A fresh verification link has been sent to your email address.') }}
         </div>
       @endif
-
       {{ __('Before proceeding, please check your email for a verification link.') }}
       {{ __('If you did not receive the email') }},
-
       <form method="POST" action="{{ route('verification.resend') }}">
       @csrf
         <div class="row">
           <!-- /.col -->
-          <div class="col-12 mt-3">
-            <button type="submit" class="btn btn-primary btn-block">{{ __('click here to request another') }}</button>
+          <div class="col-12 mt-3 text-center">
+            <button type="submit" class="btn btn-primary">{{ __('click here to request another') }}</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
-      <p class="mt-3">
-      <a href="{{ route('logout') }}" class="text-center" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Sign Out') }}</a>
-      </p>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-          @csrf
-      </form>
-    </div>
-    <!-- /.card-body -->
   </div>
-  <!-- /.card -->
+  <!-- /.card-body -->
+  <div class="card-footer">
+    Footer
+  </div>
+  <!-- /.card-footer-->
 </div>
-<!-- /.login-box -->
+<!-- /.card -->
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,30 +3,30 @@
 @section('content')
 <!-- Default box -->
 <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
+  <div class="card-header">
+    <h3 class="card-title">Title</h3>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
+    <div class="card-tools">
+      <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+        <i class="fas fa-minus"></i>
+      </button>
+      <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+        <i class="fas fa-times"></i>
+      </button>
+    </div>
+  </div>
+  <div class="card-body">
+      @if (session('status'))
+          <div class="alert alert-success" role="alert">
+              {{ session('status') }}
           </div>
-        </div>
-        <div class="card-body">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
+      @endif
+  </div>
+  <!-- /.card-body -->
+  <div class="card-footer">
+    Footer
+  </div>
+  <!-- /.card-footer-->
+</div>
+<!-- /.card -->
 @endsection

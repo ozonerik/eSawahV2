aea7c88236a29476b9af817d1c5152d9c4b6b7ae
ozonerik <ozonerik@gmail.com>
2022-04-09 12:23:31 +0700

    fixed sidebar tree

--- resources/views/components/navitem_tree.blade.php
+++ resources/views/components/navitem_tree.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item @if($open) menu-open @endif ">
-    <a href="#" class="nav-link">
+    <a href="#" class="nav-link @if($open) active @endif">
         <i class="nav-icon {{ $icon }}"></i>
         <p>{{ $name }}<i class="right fas fa-angle-left"></i></p>
     </a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <!-- Main Sidebar Container -->
         <x-sidebar>
             <x-navitem_menu name="Dashboard" routename="dashboard" icon="fas fa-tachometer-alt" />
-            <x-navitem_tree icon="fas fa-tachometer-alt" name="Menu Utama" :open="cek_currentroute(['dashboard','home'])">
+            <x-navitem_tree icon="fas fa-tachometer-alt" name="Menu Utama" :open="cek_currentroute(['routelain','routelain2'])">
                 <x-navitem_menu name="Sub Menu 1" routename="home" icon="far fa-circle nav-icon" />
                 <x-navitem_menu name="Sub Menu 2" routename="home" icon="far fa-circle nav-icon" />
             </x-navitem_tree>

96401209b13d68ab823c198b8f455143d3388438
ozonerik <ozonerik@gmail.com>
2022-04-09 11:37:02 +0700

    fix bug sidebar

--- resources/views/components/navitem_tree.blade.php
+++ resources/views/components/navitem_tree.blade.php
@@ -1,8 +1,7 @@
 <li class="nav-item @if($open) menu-open @endif ">
     <a href="#" class="nav-link">
         <i class="nav-icon {{ $icon }}"></i>
-        {{ $name }}
-        <i class="right fas fa-angle-left"></i>
+        <p>{{ $name }}<i class="right fas fa-angle-left"></i></p>
     </a>
     <ul class="nav nav-treeview">
         {{ $slot }}

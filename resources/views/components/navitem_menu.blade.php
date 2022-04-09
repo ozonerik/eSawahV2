6d8f595a96bb601a052108c704ca7ae73d9931b8
ozonerik <ozonerik@gmail.com>
2022-04-09 11:38:49 +0700

    fix bug sidebar

--- resources/views/components/navitem_menu.blade.php
+++ resources/views/components/navitem_menu.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-    <a href="#" class="nav-link @if( request()->routeIs( $routename ) ) active @endif">
+    <a href="{{ route($routename) }}" class="nav-link @if( request()->routeIs( $routename ) ) active @endif">
     <i class="nav-icon {{ $icon }}"></i>
         <p>{{ $name }}</p>
     </a>

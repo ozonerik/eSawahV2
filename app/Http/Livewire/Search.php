c602dfaa56ba5ba0e48ac079cf77de1c4b4f8990
ozonerik <ozonerik@gmail.com>
2022-04-10 07:24:47 +0700

    fix search clear

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -8,7 +8,7 @@ class Search extends Component
 {
     public $search_txt, $target_comp;
 
-    public function get_search(){     
+    private function component_target(){
         switch ($this->target_comp) {
             case "dashboard":
                 $this->emitTo('backend.dashboard', 'do_search', $this->search_txt);
@@ -23,21 +23,13 @@ class Search extends Component
                 echo "Your favorite color is neither red, blue, nor green!";
         }
     }
+    
+    public function get_search(){     
+        $this->component_target();
+    }
     public function clearSearch(){     
         $this->search_txt = '';
-        switch ($this->target_comp) {
-            case "dashboard":
-                $this->emitTo('backend.dashboard', 'do_search', $this->search_txt);
-                break;
-            case "blue":
-                echo "Your favorite color is blue!";
-                break;
-            case "green":
-                echo "Your favorite color is green!";
-                break;
-            default:
-                echo "Your favorite color is neither red, blue, nor green!";
-        }
+        $this->component_target();
     }
 
     public function render()

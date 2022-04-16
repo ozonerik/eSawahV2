f34f0ff735c5d72c762b0106a0ab180f84ef9af8
ozonerik <ozonerik@gmail.com>
2022-04-17 06:51:28 +0700

    no result msg

--- resources/views/livewire/backend/result.blade.php
+++ resources/views/livewire/backend/result.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <x-content_header name="Result" >
-        <li class="breadcrumb-item active">Result</a></li>
+    <x-content_header name="Search result" >
+        <li class="breadcrumb-item active">Search result</a></li>
     </x-content_header>
     <div class="row mx-1">
         <x-card_list width="12" title="{{ $user->count() }} result searching for '{{ $search }}' ">
@@ -9,7 +9,7 @@
             <x-card_listitem photo="{{asset('dist/img/default-150x150.png')}}" link="#" title="{{$row->name}}" desc="Sawah Mundu" value="1.000.000" />
             @endforeach
         @else
-            <h4 class="text-center text-info my-5"> Sorry, No Result Found </h4>
+            <h4 class="text-center text-info my-5"> Sorry, No result found ! </h4>
         @endif
         </x-card_list>
     </div>

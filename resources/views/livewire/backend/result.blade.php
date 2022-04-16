282ba76a660e50474cd49c6f6a10e908f8ba7fd0
ozonerik <ozonerik@gmail.com>
2022-04-17 06:44:32 +0700

    no result msg

--- resources/views/livewire/backend/result.blade.php
+++ resources/views/livewire/backend/result.blade.php
@@ -3,7 +3,7 @@
         <li class="breadcrumb-item active">Result</a></li>
     </x-content_header>
     <div class="row mx-1">
-        <x-card_list width="12" title="Search Result">
+        <x-card_list width="12" title="{{ $user->count() }} result searching for {{ $search }}">
         @if(!empty($user->toArray()))
             @foreach($user as $row)
             <x-card_listitem photo="{{asset('dist/img/default-150x150.png')}}" link="#" title="{{$row->name}}" desc="Sawah Mundu" value="1.000.000" />

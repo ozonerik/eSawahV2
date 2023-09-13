e6732529b28d84c878970791032616e9f13a9fb0
ozonerik <ozonerik@gmail.com>
2023-09-13 13:02:49 +0700

    fix profile edit

--- app/Http/Livewire/Backend/Profile.php
+++ app/Http/Livewire/Backend/Profile.php
@@ -3,12 +3,44 @@
 namespace App\Http\Livewire\Backend;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class Profile extends Component
 {
+    public $mode='read';
+    public $user,$name,$email;
 
     public function render()
     {
+        $user=User::find(Auth::user()->id);
         return view('livewire.backend.profile')->extends('layouts.app');
+    }
+    
+    public function mount()
+    {
+        $this->resetErrorBag();
+        $this->resetValidation();
+        $user = User::find(Auth::user()->id);          
+        if($user) {
+            $this->name    = $user->name;
+            $this->email  = $user->email;
+        }
+    }
+
+    public function updateprofile()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . Auth::user()->id
+        ]);
+        User::updateOrCreate(['id' => Auth::user()->id], [
+            'name' => $this->name,
+            'email' => $this->email,
+        ]);
+        //flash message
+        session()->flash('success', 'Profile Berhasil Diupdate.');
+        //redirect
+        return redirect()->route('profile');
     }
 }
--- resources/views/livewire/backend/profile.blade.php
+++ resources/views/livewire/backend/profile.blade.php
@@ -5,17 +5,29 @@
     <div class="row mx-1 pb-3">
         <x-card_profile name="Profile Picture" width="3" order="1" smallorder="1"/>
         <x-card_section2 name="Profile Information" type="primary" width="9" order="2" smallorder="2">
+        <form wire:submit.prevent="updateprofile">
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" placeholder="Enter Name" value="{{ucwords(Auth::user()->name)}}">
+                <input type="text" wire:model="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter Name" >
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                    </span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="email">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter email" value="{{ucwords(Auth::user()->email)}}">
+                <input type="email" wire:model="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Enter email" >
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        {{ $message }}
+                    </span>
+                @enderror
             </div>
             <div class="form-group text-md-right text-center">
-                <button wire:click="onRead" class="btn btn-primary">Save</button>
+                <button type="submit" class="btn btn-primary">Save</button>
             </div>
+        </form>
         </x-card-section2>
     </div>
     <div class="row mx-1 pb-3">

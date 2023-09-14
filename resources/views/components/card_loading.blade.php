90fcd9ce57d570827d7eda6e58bf8f4e49d23b8a
ozonerik <ozonerik@gmail.com>
2023-09-15 00:12:49 +0700

    perbaikan custom file

--- resources/views/components/card_loading.blade.php
+++ resources/views/components/card_loading.blade.php
@@ -1,3 +1,4 @@
 <div class="overlay">
+    <!-- add before .card -->
     <i class="fas fa-2x fa-sync-alt fa-spin"></i>
 </div>
--- resources/views/components/file_form.blade.php
+++ resources/views/components/file_form.blade.php
@@ -1,8 +1,7 @@
 <div class="form-group">
     <div class="custom-file" wire:ignore>
-        {{ $errors->first($name) }}
-        <input type="file" id="{{ $ids }}"  wire:model="{{ $name }}" wire:target="{{ $name }}" wire:loading.attr="disabled" class="custom-file-input form-control  @if ($message = Session::get($name)) is-invalid @endif" >
-        <label class="custom-file-label" for="{{ $ids }}">{{ $label }}</label>
+        <input type="file" wire:model="{{ $name }}" wire:target="{{ $name }}" wire:loading.attr="disabled" class="custom-file-input @if ($message = Session::get($name)) is-invalid @endif" id="{{ $ids }}" aria-describedby="{{ $ids }}">
+        <label class="custom-file-label" for="{{ $ids }}">Choose file</label>
         @if ($message = Session::get($name))
             <span class="invalid-feedback" role="alert">
                 {{ $message }}

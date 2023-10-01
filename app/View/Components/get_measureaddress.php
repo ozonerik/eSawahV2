4530a6fa47d4aff1c0227584884bb062a8b65836
ozonerik <ozonerik@gmail.com>
2023-10-01 23:22:55 +0700

    GIS auto complete adress fixed 2

---
+++ app/View/Components/get_measureaddress.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class get_measureaddress extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $eventname,$emitname,$inputname,$mapname,$kordinatname;
+    public function __construct($eventname,$emitname,$inputname,$mapname,$kordinatname)
+    {
+        $this->eventname = $eventname;
+        $this->emitname = $emitname;
+        $this->inputname = $inputname;
+        $this->mapname = $mapname;
+        $this->kordinatname = $kordinatname;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.get_measureaddress');
+    }
+}
---
+++ resources/views/components/get_measureaddress.blade.php
@@ -0,0 +1,47 @@
+<script>
+document.addEventListener('livewire:load', function () {
+    Livewire.emit('onGetAdress');
+})
+</script>
+<script>
+document.addEventListener(@js($eventname), event => {
+    console.log('hallo');
+    initAutocomplete();
+    var ac=90;
+    var lt=event.detail.lt;
+    var lg=event.detail.lg;
+    var map_id=event.detail.map_id;
+    var kordinat=event.detail.kordinat;
+    var mapname=@js($mapname)+'-'+map_id;
+    if (!(lt === undefined || lg === undefined || lt === 0 || lg === 0)){
+        showMeasureMaps(@js($emitname),lt,lg,ac,mapname,'true',kordinat);
+    }else{
+        showMeasureMaps(@js($emitname),lt,lg,ac,'nomap'+'-'+map_id,'true','NoMap');
+    }
+});
+
+async function initAutocomplete() {
+        var input = document.getElementById(@js($inputname));
+        const options = {
+            componentRestrictions: { country: "id" },
+            fields: ["formatted_address", "geometry", "name"],
+        };
+        var autocomplete = new google.maps.places.Autocomplete(input, options);
+        autocomplete.addListener('place_changed', function () {
+            var place = autocomplete.getPlace();
+            if(!place.geometry){
+                var lokasi=document.getElementById(@js($inputname)).value;
+                @this.set(@js($inputname), lokasi);
+                @this.set(@js($kordinatname),'');
+                Livewire.emit(@js($emitname), {'lat': 0, 'long': 0, 'lokasi':lokasi});
+            }else{
+                var lt=place.geometry['location'].lat();
+                var lg=place.geometry['location'].lng();
+                var lokasi=document.getElementById(@js($inputname)).value;
+                @this.set(@js($inputname), lokasi);
+                var ac=90;
+                Livewire.emit(@js($emitname), {'lat': lt, 'long': lg, 'lokasi':lokasi});
+            }
+        }); 
+}
+</script>

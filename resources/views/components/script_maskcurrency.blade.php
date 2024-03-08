a971efb0d9617ed5626d59c3f54db0bee4ada353
ozonerik <ozonerik@gmail.com>
2024-03-08 20:29:04 +0700

    componen script currency2

---
+++ resources/views/components/script_maskcurrency.blade.php
@@ -0,0 +1,22 @@
+<script>
+window.addEventListener('run_maskcurrency', event => {
+    document.querySelectorAll('input[type-currency="IDR"]').forEach((element) => {
+        element.addEventListener('keyup', function(e) {
+        let cursorPostion = this.selectionStart;
+            let value = parseInt(this.value.replace(/[^,\d]/g, ''));
+            let originalLenght = this.value.length;
+            if (isNaN(value)) {
+            this.value = "";
+            } else {    
+            this.value = value.toLocaleString('id-ID', {
+                currency: 'IDR',
+                style: 'currency',
+                minimumFractionDigits: 0
+            });
+            cursorPostion = this.value.length - originalLenght + cursorPostion;
+            this.setSelectionRange(cursorPostion, cursorPostion);
+            }
+        });
+    });
+})
+</script>

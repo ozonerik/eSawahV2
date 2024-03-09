aad774d582adceb8796283137463479d72aba309
ozonerik <ozonerik@gmail.com>
2024-03-09 22:55:22 +0700

    select2 multi fixed4

--- resources/views/components/dropdown_select2_multi.blade.php
+++ resources/views/components/dropdown_select2_multi.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <label for="{{$ids}}" >{{$label}}</label>
     <div class="select2bs4-blue" wire:ignore>
-        <select wire:model="{{$name}}" multiple="multiple" id="{{$ids}}" style="width:100%" data-dropdown-css-class="select2bs4-blue" class="form-control select2bs4_multi @if($errors->has( $name )) is-invalid @endif" >
+        <select wire:model="{{$name}}" multiple="multiple" id="{{$ids}}" style="width:100%" data-dropdown-css-class="select2bs4-blue" class="form-control @if($errors->has( $name )) is-invalid @endif" >
             <option value="">Please Choose...</option> 
             @foreach ($data as $row)
             <option value="{{$row->$values}}" > {{$row->$showval}}</option>

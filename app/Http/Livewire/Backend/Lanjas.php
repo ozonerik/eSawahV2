49b1a1ca2ffd81474ba2288eba639b3953ec39d5
ozonerik <ozonerik@gmail.com>
2024-03-11 07:19:20 +0700

    percobaan input mask

--- app/Http/Livewire/Backend/Lanjas.php
+++ app/Http/Livewire/Backend/Lanjas.php
@@ -9,7 +9,7 @@ use Carbon\Carbon;
 
 class Lanjas extends Component
 {
-    public $pawongan,$opsipawongan,$opsipawonganbs4,$luas,$tanggal;
+    public $pawongan,$opsipawongan,$opsipawonganbs4,$luas,$bata,$tanggal;
     public $opsipawonganmulti=[];
 
     public function addLanja(){
@@ -17,18 +17,16 @@ class Lanjas extends Component
         $luas=Manny::stripper($this->luas,['num','comma']);
         $tanggal=Carbon::parse($this->tanggal)->format("Y-m-d");
     }
+    public function updatedLuas($value){
+        $this->bata=conv_inputmask($value);
+    }
     public function mount(){
-        $this->dispatchBrowserEvent('run_select2');
         $this->pawongan=Pawongan::get();
         $this->luas='0';
         $this->tanggal=Carbon::parse(now())->format("d/m/Y");;
-        $string='1.200,45 m2';
-        $result= conv_inputmask($string);
-        dd($result);
     }
     public function render()
     {
-        //$data['pawongan']= $this->pawongan;
         $this->dispatchBrowserEvent('run_select2');
         return view('livewire.backend.lanjas')->extends('layouts.app');
     }
--- resources/views/components/input_mask.blade.php
+++ resources/views/components/input_mask.blade.php
@@ -2,25 +2,25 @@
     <label for="{{ $ids }}" class="@if(!empty($wajib)) text-danger  @endif">{{ $label }}</label>
     
     @if($typemask == 'harga')
-    <input data-inputmask="'shortcuts':{'r': '1000', 'j': '1000000','m':'1000000000','t':'1000000000000'},'prefix': 'Rp ','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 0, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'shortcuts':{'r': '1000', 'j': '1000000','m':'1000000000','t':'1000000000000'},'prefix': 'Rp ','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 0, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'luas')
-    <input data-inputmask="'suffix': ' m2','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'suffix': ' m2','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'panjang')
-    <input data-inputmask="'suffix': ' m','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'suffix': ' m','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'bata')
-    <input data-inputmask="'suffix': ' bata','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'suffix': ' bata','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'bau')
-    <input data-inputmask="'suffix': ' bau','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'suffix': ' bau','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'hektar')
-    <input data-inputmask="'suffix': ' ha','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'suffix': ' ha','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'kwintal')
-    <input data-inputmask="'suffix': ' kw','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'suffix': ' kw','alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'desimal')
-    <input data-inputmask="'alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false " data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'alias': 'decimal', 'radixPoint':',', 'groupSeparator': '.', 'autoGroup': true, 'digits': 2, 'digitsOptional': false, 'rightAlign': false "  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'telp')
-    <input data-inputmask="'mask': ['9999-9999-999[9][9][9]']" data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask="'mask': ['9999-9999-999[9][9][9]']" @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @elseif($typemask == 'tanggal')
-    <input data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control datepicker @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" autocomplete="off" 
+    <input data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy"  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control datepicker @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" autocomplete="off" 
     data-provide="datepicker" 
     data-date-language="id" 
     data-date-autoclose="true" 
@@ -30,7 +30,7 @@
     
     >
     @elseif($typemask == 'email')
-    <input data-inputmask-alias="email" data-mask @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
+    <input data-inputmask-alias="email"  @if(!empty($wajib)) requiered @endif @if($disabled=="true") disabled @endif id="{{ $ids }}" type="{{ $types }}" wire:model="{{ $name }}" class="form-control @if($errors->has( $name )) is-invalid @endif"  placeholder="{{ $placeholder }}" >
     @endif
     
     @if($errors->has( $name ))
@@ -41,10 +41,10 @@
 </div>
 <script>
 document.addEventListener('livewire:load', function () {
-    $('#{{ $ids }}').inputmask();
-    $('#{{ $ids }}').change(function (e) {
-        let data = $(this).val();
-        @this.set('{{$name}}', data);
+    //$('#{{ $ids }}').inputmask();
+    $('#{{ $ids }}').on('keyup',function (e) {
+        //let data = $(this).val();
+        //@this.set('{{$name}}', data);
     });
 })
 window.addEventListener('run_inputmask', event => {
